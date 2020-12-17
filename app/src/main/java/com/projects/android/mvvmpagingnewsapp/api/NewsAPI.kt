8261d6ba8225a6d4fbcd1c45bbfef72ebdd281d2
Anmol <sahi.anmool@gmail.com>
2020-12-17 20:56:27 +0530

    Added the NewsAPI.kt intergface file

---
+++ app/src/main/java/com/projects/android/mvvmpagingnewsapp/api/NewsAPI.kt
@@ -0,0 +1,24 @@
+package com.projects.android.mvvmpagingnewsapp.api
+
+import com.projects.android.mvvmpagingnewsapp.BuildConfig
+import com.projects.android.mvvmpagingnewsapp.models.ArticleResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsAPI {
+
+    @GET("/v2/top-headlines")
+    suspend fun getBreakingNews(
+        @Query("country") countryCode: String,
+        @Query("page") pageNumber: Int,
+        @Query("apiKey") apiKey: String = BuildConfig.NEWS_ACCESS_KEY
+    ): ArticleResponse
+
+    @GET("/v2/everything")
+    suspend fun searchNews(
+        @Query("q") query: String,
+        @Query("page") pageNumber: Int,
+        @Query("apiKey") apiKey: String = BuildConfig.NEWS_ACCESS_KEY
+    )
+
+}
--- app/src/main/java/com/projects/android/mvvmpagingnewsapp/models/ArticleResponse.kt
+++ app/src/main/java/com/projects/android/mvvmpagingnewsapp/models/ArticleResponse.kt
@@ -3,7 +3,7 @@ package com.projects.android.mvvmpagingnewsapp.models
 
 import com.google.gson.annotations.SerializedName
 
-data class Response(
+data class ArticleResponse(
     @SerializedName("articles")
     val articles: List<Article>,
     @SerializedName("status")
