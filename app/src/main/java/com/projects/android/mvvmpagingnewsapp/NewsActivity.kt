8da2892f665654ec1c7e20d3494ee23f20fc9707
Anmol <sahi.anmool@gmail.com>
2020-12-17 20:14:39 +0530

    initial commit - project setup

---
+++ app/src/main/java/com/projects/android/mvvmpagingnewsapp/NewsActivity.kt
@@ -0,0 +1,11 @@
+package com.projects.android.mvvmpagingnewsapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class NewsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
